5d3650bf9fb66092904dcf4268a42e706a532938
0kate <o.keito317@icloud.com>
2023-01-01 14:37:05 +0900

    update & add bytecode

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
-;; Packages
+;; Profiler
+(require 'profiler)
+(profiler-start 'cpu)
+
+;; Packages manager
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
@@ -6,167 +10,180 @@
 (package-initialize)
 (package-refresh-contents)
 
-(defun package-ensure-package (package)
-  (unless (package-installed-p package)
-    (package-install package)))
+(eval-when-compile
+  (unless (package-installed-p 'use-package)
+    (message "use-package is not ready. Installing...")
+    (package-install 'use-package)
+    (message "use-package is installed!"))
+  (require 'use-package))
 
-;; monokai-theme
-(package-ensure-package 'monokai-theme)
-(load-theme 'monokai t)
 
-;; exec-path-from-shell
-(package-ensure-package 'exec-path-from-shell)
-(exec-path-from-shell-copy-envs '("ASDF_DIR" "ASDF_DATA_DIR"))
-(exec-path-from-shell-initialize)
 
-;; direnv
-(package-ensure-package 'direnv)
-(direnv-mode)
+;; Packages (Enhancements)
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
 
-;; neotree
-(package-ensure-package 'neotree)
-(setq neo-theme 'nerd
-      neo-window-fixed-size nil
-      neo-autorefresh t)
-(global-set-key (kbd "C-t") 'neotree-toggle)
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-copy-envs '("ASDF_DIR" "ASDF_DATA_DIR"))
+  (exec-path-from-shell-initialize))
 
-;; whitespace
-(package-ensure-package 'whitespace)
-(global-whitespace-mode 1)
-(setq whitespace-line nil
-      whitespace-display-mappings '((space-mark   ?\x3000 [?\▫])
-                                    (tab-mark     ?\t     [?\xBB ?\t])
-                                    (newline-mark ?\n     [?¬ ?\n])))
+(use-package neotree
+  :ensure t
+  :init
+  (setq neo-theme 'nerd
+        neo-window-fixed-size nil
+        neo-autorefresh t)
+  :bind (("C-t" . neotree-toggle)))
 
-;; highlight-indent-guides
-(package-ensure-package 'highlight-indent-guides)
-(setq highlight-indent-guides-method 'character
-      highlight-indent-guides-character ?┊)
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-(add-hook 'text-mode-hook 'highlight-indent-guides-mode)
+(use-package git-gutter
+  :ensure t
+  :init
+  (setq git-gutter:modified-sign "~")
+  :config
+  (global-git-gutter-mode t))
 
-;; rainbow-delimiters
-(package-ensure-package 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(use-package whitespace
+  :init
+  (setq whitespace-line nil
+        whitespace-display-mappings '((space-mark   ?\x3000 [?\▫])
+                                      (tab-mark     ?\t     [?\xBB ?\t])
+                                      (newline-mark ?\n     [?¬ ?\n])))
+  :config
+  (global-whitespace-mode 1))
 
-;; magit
-(package-ensure-package 'magit)
+(use-package highlight-indent-guides
+  :ensure t
+  :init
+  (setq highlight-indent-guides-method 'character
+        highlight-indent-guides-character ?┊))
 
-;; git-gutter
-(package-ensure-package 'git-gutter)
-(setq git-gutter:modified-sign "~")
-(global-git-gutter-mode t)
+(use-package rainbow-mode :ensure t)
 
-;; company
-(package-ensure-package 'company)
-(global-company-mode t)
-(setq company-backends '((company-capf company-dabbrev-code))
-      company-minimum-prefix-length 1
-      company-idle-delay 0)
+(use-package rainbow-delimiters :ensure t)
 
-;; restclient
-(package-ensure-package 'restclient)
+(use-package company
+  :ensure t
+  :init
+  (setq company-backends '((company-capf company-dabbrev-code))
+        company-minimum-prefix-length 1
+        company-idle-delay 0)
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
 
-;; company-restclient
-(package-ensure-package 'company-restclient)
-(add-to-list 'company-backends 'company-restclient)
+(use-package restclient :ensure t)
+(use-package company-restclient
+  :ensure t
+  :after (company)
+  :config
+  (add-to-list 'company-backends 'company-restclient))
 
-;; flycheck
-(package-ensure-package 'flycheck)
-(global-flycheck-mode)
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-center-theme))
 
-;; flycheck-rust
-(package-ensure-package 'flycheck-rust)
-(add-hook 'rust-mode-hook 'flycheck-rust-setup)
+(use-package flycheck
+  :ensure t
+  :config (global-flycheck-mode))
 
-;; ivy
-(package-ensure-package 'ivy)
-(ivy-mode t)
+(use-package flycheck-rust
+  :ensure t
+  :after (flycheck)
+  :config
+  (add-hook 'rust-mode-hook 'flycheck-rust-setup))
 
-;; yaml-mode
-(package-ensure-package 'yaml-mode)
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode t))
 
-;; rust-mode
-(package-ensure-package 'rust-mode)
 
-;; docker
-(package-ensure-package 'docker)
-(package-ensure-package 'dockerfile-mode)
 
-;; web-mode
-(package-ensure-package 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(setq web-mode-attr-indent-offset nil
-      web-mode-markup-indent-offset 2
-      web-mode-css-indent-offset 2
-      web-mode-code-indent-offset 2
-      web-mode-enable-current-element-highlight t)
-(setq web-mode-enable-auto-closing t
-      web-mode-enable-auto-pairing t
-      web-mode-auto-close-style 2
-      web-mode-tag-auto-close-style 2)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-html-tag-bracket-face ((t (:foreground "#909090")))))
+;; Packages (Integrations)
+(use-package direnv
+  :ensure t
+  :config
+  (direnv-mode))
 
-;; json-mode
-(package-ensure-package 'json-mode)
-(setq js-indent-level 2)
+(use-package magit :ensure t)
 
-;; svelte-mode
-(package-ensure-package 'svelte-mode)
-(customize-set-variable 'svelte-basic-offset 2)
+(use-package yaml-mode :ensure t)
+(use-package rust-mode :ensure t)
+(use-package docker :ensure t)
+(use-package dockerfile-mode :ensure t)
+(use-package typescript-mode :ensure t)
 
-(package-ensure-package 'tide)
-(setq tide-completion-ignore-case t
-      tide-completion-show-source t
-      tide-completion-fuzzy t
-      tide-completion-detailed t)
-(add-hook 'web-mode-hook (lambda ()
-                      (tide-setup)))
+(use-package web-mode
+  :ensure t
+  :init
+  (setq web-mode-attr-indent-offset nil
+        web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-enable-current-element-highlight t)
+  (setq web-mode-enable-auto-closing t
+        web-mode-enable-auto-pairing t
+        web-mode-auto-close-style 2
+        web-mode-tag-auto-close-style 2)
+  :mode (("\\.ts\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode))
+  :custom-face
+  (web-mode-html-tag-bracket-face ((t (:foreground "#909090")))))
 
-;; rainbow-mode
-(package-ensure-package 'rainbow-mode)
-(add-hook 'prog-mode-hook 'rainbow-mode)
-(add-hook 'text-mode-hook 'rainbow-mode)
+(use-package json-mode
+  :ensure t
+  :init
+  (setq js-indent-level 2))
 
-;; lsp-mode
-(package-ensure-package 'lsp-mode)
-(setq lsp-keymap-prefix "C-c l")
-;; for debug
-;; (setq lsp-print-io t)
-;; for clangd
-(setq lsp-clangd-binary-path (executable-find "clangd"))
-;; for rust-analyzer
-(setq lsp-rust-analyzer-display-parameter-hints t
-      lsp-rust-analyzer-binding-mode-hints t
-      lsp-rust-analyzer-inlay-hints-mode t
-      lsp-rust-analyzer-display-lifetime-elision-hints-enable t
-      lsp-rust-analyzer-server-display-inlay-hints t)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'ruby-mode-hook 'lsp)
-(add-hook 'rust-mode-hook 'lsp)
-(add-hook 'yaml-mode-hook 'lsp)
-(add-hook 'dockerfile-mode 'lsp)
-(add-hook 'svelte-mode-hook 'lsp)
+(use-package svelte-mode
+  :ensure t
+  :custom
+  (svelte-basic-offset 2))
 
-;; lsp-ui
-(package-ensure-package 'lsp-ui)
-(setq lsp-ui-sideline-show-diagnostics t
-      lsp-ui-sideline-show-code-actions t)
+(use-package tide
+  :ensure t
+  :after (typescript-mode)
+  :init
+  (setq tide-completion-ignore-case t
+        tide-completion-show-source t
+        tide-completion-fuzzy t
+        tide-completion-detailed t)
+  :hook ((web-mode . (lambda () (tide-setup)))))
 
-;; powerline
-(package-ensure-package 'powerline)
-(powerline-center-theme)
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; for debugging
+  ;; (setq lsp-print-io t)
 
-;; restclient
-(package-ensure-package 'restclient)
-(package-ensure-package 'company-restclient)
-(add-to-list 'company-backends 'company-restclient)
+  ;; for clangd
+  (setq lsp-clangd-binary-path (executable-find "clangd"))
+
+  ;; for rust-analyzer
+  (setq lsp-rust-analyzer-display-parameter-hints t
+        lsp-rust-analyzer-binding-mode-hints t
+        lsp-rust-analyzer-inlay-hints-mode t
+        lsp-rust-analyzer-display-lifetime-elision-hints-enable t
+        lsp-rust-analyzer-server-display-inlay-hints t)
+  :hook ((c-mode . lsp)
+         (ruby-mode . lsp)
+         (rust-mode . lsp)
+         (yaml-mode . lsp)
+         (dockerfile-mode . lsp)
+         (svelte-mode . lsp)))
+
+(use-package lsp-ui
+  :ensure t
+  :after (lsp-mode)
+  :init
+  (setq lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-code-actions t))
+
+
 
 ;; Hooks
 (defun disable-scroll-margin ()
@@ -176,9 +193,6 @@
 
 (add-hook 'window-setup-hook '(lambda ()
                                 (set-face-background 'default "undefined")))
-(add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'eshell-mode-hook '(lambda ()
-                               (display-line-numbers-mode -1)))
 (add-hook 'eshell-mode-hook '(lambda ()
                                (display-line-numbers-mode -1)
                                (eshell-disable-buffer-control)))
@@ -187,6 +201,8 @@
 (add-hook 'prog-mode-hook '(lambda () (disable-scroll-margin)))
 (add-hook 'text-mode-hook '(lambda () (disable-scroll-margin)))
 (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+
+
 
 ;; Basic options
 (setq-default indent-tabs-mode nil)
@@ -204,6 +220,12 @@
 
 ;; Basic keybinds
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Debug options
+;; (setq max-specpdl-size 5
+;;       debug-on-error t)
+
+
 
 ;; Custsom commands
 (defun restclient-new-buffer ()
@@ -247,28 +269,29 @@
   (interactive)
   (split-window-vertically))
 
-;; (setq copy-command '("xclip")
-;;       paste_command "xclip -o | tr -d \r"
-;;       copy-process nil)
+(setq copy-command '("xclip")
+      paste_command "xclip -o | tr -d \r"
+      copy-process nil)
 
-;; (defun copy-to-clipboard (text)
-;;   (setq copy-process (make-process :name "copy-proc"
-;;                                    :buffer nil
-;;                                    :command copy-command
-;;                                    :connection-type 'pipe))
-;;   (process-send-string copy-process text)
-;;   (process-send-eof copy-process))
+(defun copy-to-clipboard (text)
+  (setq copy-process (make-process :name "copy-proc"
+                                   :buffer nil
+                                   :command copy-command
+                                   :connection-type 'pipe))
+  (process-send-string copy-process text)
+  (process-send-eof copy-process))
 
-;; (defun paste-from-clipboard ()
-;;   (if (and copy-process (process-live-p copy-process))
-;;       nil
-;;     (shell-command-to-string paste_command)))
+(defun paste-from-clipboard ()
+  (if (and copy-process (process-live-p copy-process))
+      nil
+    (shell-command-to-string paste_command)))
 
-;; (setq interprogram-cut-function 'copy-to-clipboard)
-;; (setq interprogram-paste-function 'paste-from-clipboard)
+(setq interprogram-cut-function 'copy-to-clipboard)
+(setq interprogram-paste-function 'paste-from-clipboard)
 
 ;; asdf enable
-(let ((path (substitute-env-vars (concat (concat (if (getenv "ASDF_DATA_DIR") "$ASDF_DATA_DIR" "$HOME") "/.asdf/shims")
+(let ((path (substitute-env-vars (concat (if (getenv "ASDF_DATA_DIR") "$ASDF_DATA_DIR" "$HOME")
+                                         "/.asdf/shims"
                                          ":$HOME/.asdf/bin:$PATH"))))
   (setenv "PATH" path)
   (setq exec-path
@@ -277,28 +300,24 @@
          exec-path)))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eshell-prompt-function
-   (lambda nil
-     (concat
-      (propertize
-       (getenv "USER")
-       'face
-       '(:foreground "green" :bold t))
-      (propertize "@" 'face
-                  '(:foreground "white"))
-      (system-name)
-      (propertize ":" 'face
-                  '(:foreground "white"))
-      (eshell/pwd)
-      (propertize "  " 'face
-                  '(:foreground "white"))
-      (magit-get-current-branch)
-      (propertize "  " 'face
-                  '(:foreground "white")))))
- '(eshell-prompt-regexp ".*  ")
- '(package-selected-packages
-   '(org-preview-html all-the-icons-ibuffer all-the-icons json-mode restclient use-package monokai-theme magit lsp-mode company)))
+ '(eshell-prompt-function (lambda nil
+                            (concat
+                             (propertize
+                              (getenv "USER")
+                              'face
+                              '(:foreground "green" :bold t))
+                             (propertize "@" 'face
+                                         '(:foreground "white"))
+                             (system-name)
+                             (propertize ":" 'face
+                                         '(:foreground "white"))
+                             (eshell/pwd)
+                             (propertize "  " 'face
+                                         '(:foreground "white"))
+                             (magit-get-current-branch)
+                             (propertize "  " 'face
+                                         '(:foreground "white")))))
+ '(eshell-prompt-regexp ".*  "))
+
+(profiler-report)
+(profiler-stop)
